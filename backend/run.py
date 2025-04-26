74d6d91004a70c1badb2814f56b674bbbb268eab
harshit <156461915+Zukliod@users.noreply.github.com>
2025-04-26 18:37:31 +0530

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,12 @@
-from app import create_app
+import os
+from flask import Flask
 
-app = create_app()
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Railway!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
